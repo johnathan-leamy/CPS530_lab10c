ab22c7c818ac55a25e0751d87f636e6c161d8cd7
johnathan-leamy <johnathan.leamy@ryerson.ca>
2022-12-03 00:41:55 -0500

    new component

--- src/App.js
+++ src/App.js
@@ -18,9 +18,20 @@ function App() {
           Learn React
         </a>
       </header> */}
-      this is my div and this is the only div that I need
+      <customHeader/>
+      <p>Since react is a very widely used front end framework it was very easy to find a web hosting service which supports react.</p>
+      <p>When it comes to installing react that was also very easy as all it took was going to the react website then type the given commands into the terminal</p>
+      <p>By using vercel the prosses of setting up the webpage involved upload the project to my personal github page then selecting it in the vercel website</p> 
+      <p>The hardest part of setting up react was finding a good webhosting site as I originally tried to use the moons server but due to not being able to 
+        use the create-react app comand with npm. To solve this issue I searched for webhosting sites that work specificly with react, found a top ten list and clicked the site that was number one.
+      </p>
     </div>
   );
+}
+function customHeader(){
+  return(
+  <div>React in Vercel</div>
+  )
 }
 
 export default App;
